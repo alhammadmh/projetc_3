f2979a95ca538ab63621062e29458ba81398c0ea
mohammed <alhammad2013@gmail.com>
2019-04-21 10:55:07 +0300

    files merge

--- db/migrate/20190421073246_create_students.rb
+++ db/migrate/20190421073246_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.2]
   def change
     create_table :students do |t|
-      t.strig :name
+      t.string :name
       t.integer :course_id
 
       t.timestamps
