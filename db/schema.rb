66a32539f373b49fd0e38ea21b7fe1c5fb759d01
MacBook Pro <macbookpro@Sarah.local>
2019-04-21 12:13:57 +0300

    add 3 couses to database + make card coouse

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_21_073246) do
+ActiveRecord::Schema.define(version: 2019_04_21_083225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,12 +18,13 @@ ActiveRecord::Schema.define(version: 2019_04_21_073246) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.text "img"
-    t.date "date"
     t.string "type"
     t.text "desc"
     t.integer "instructor_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "start"
+    t.datetime "end"
   end
 
   create_table "instructors", force: :cascade do |t|
