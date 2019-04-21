84aba5c77ff8931cb2bdf562f91d452a1eb68966
MacBook Pro <macbookpro@Sarah.local>
2019-04-21 10:44:43 +0300

    couse model and controller done

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,39 @@
+class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new 
+    @course = Course.new
+  end
+
+  def create
+    course= Course.create(course_params)
+    redirect_to courses_path
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+
+  end
+
+  def update
+    course = Course.find(params[:id])
+    course.update(course_params) 
+    redirect_to course
+  end
+
+  def destroy
+  end
+
+  private
+
+  def course_params
+  params.require(:course).permit(:name, :img, :date, :type, :desc, :instructor_id)
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'courses/index'
+  get 'courses/show'
+  get 'courses/new'
+  get 'courses/_form'
+  get 'courses/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
