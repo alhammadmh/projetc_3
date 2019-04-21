6149a0ff6743b9d08bf91b80488861a00e3eb413
bob <bob@>
2019-04-21 23:09:19 +0300

    add sign up login logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
     # before_action :authenticate_user!, :except  [:index]
+    protect_from_forgery with: :exception
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protected
+  
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_type])
+            devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :user_type])
+            devise_parameter_sanitizer.permit(:account_update, keys: [:name, :user_type])
+      end
+
+
+
 end
