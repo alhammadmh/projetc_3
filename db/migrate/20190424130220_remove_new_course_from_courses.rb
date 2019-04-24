495d662a96953aedeccce09f0606656fc6bf4d08
mohammed <alhammad2013@gmail.com>
2019-04-24 16:50:17 +0300

    last update

--- db/migrate/20190424130220_remove_new_course_from_courses.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNewCourseFromCourses < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :courses, :new_course, :boolean
-  end
-end
--- db/migrate/20190424130321_remove_new_from_courses.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNewFromCourses < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :courses, :new, :boolean
-  end
-end
