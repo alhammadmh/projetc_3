ef3e5d1080c1b550cfd3dee078f9086da4ddaeed
MacBook Pro <macbookpro@Sarah.local>
2019-04-24 15:01:35 +0300

    fix create course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,5 @@
 class Course < ApplicationRecord
-    # belongs_to :user
-belongs_to :instructor, class_name: 'User', foreign_key: 'instructor_id'
-belongs_to :users
+  belongs_to :instructor, class_name: 'User', foreign_key: 'instructor_id'
 
     def self.search(search)
         if search
