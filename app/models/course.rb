6201de3bf1890d9391a08337c9a8ce71216f4c7d
MacBook Pro <macbookpro@Sarah.local>
2019-04-24 12:32:39 +0300

    join edit delete course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
     # belongs_to :user
 belongs_to :instructor, class_name: 'User', foreign_key: 'instructor_id'
-  has_and_belongs_to_many :users, join_table: :courses_students, foreign_key: "student_id"
+belongs_to :users
 
     def self.search(search)
         if search
